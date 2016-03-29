30ad397d30e251fb14a39180f630215c46e1580b
Tony Won <tonywon89@gmail.com>
2016-03-29 11:25:48 -0700

    Create Cat Model And Migrations

---
+++ app/models/cat.rb
@@ -0,0 +1,17 @@
+class Cat < ActiveRecord::Base
+  COLORS  =[
+    "black",
+    "orange",
+    "tabby",
+    "pink"
+  ]
+
+  validates :birth_date, :name, :color, :sex, :description, presence: true
+  validates :color, inclusion: { in: COLORS,
+    message: "%{value} is not a valid color"}
+
+  validates :sex, length: { maximum: 1 }, inclusion: { in: ["M", "F"],
+    message: "That's not a sex! Silly billy."}
+
+
+end
